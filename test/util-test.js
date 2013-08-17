131fef2fee45074f27f392ef8cef9541619d470a
Boaz Rosenan <brosenan@gmail.com>
2013-08-17 20:03:55 +0300

    Refactoring of the representation.  Timestamp is now a time-uid represented as string.  Order should still be enforced.

---
+++ test/util-test.js
@@ -0,0 +1,53 @@
+var util = require('../util.js');
+var assert = require('assert');
+
+describe('seq(funcs, done)', function() {
+	it('should return a function that runs asynchronous functions in funcs in order', function(done) {
+		var d;
+		var f = util.seq([
+			function(_) {d = done; setTimeout(_, 10);},
+			function(_) {d();}
+		], function() {});
+		f();
+	});
+	it('should handle errors by calling done with the error', function(done) {
+		util.seq([
+			function(_) {_(new Error('someError'));},
+			function(_) {assert(0, 'This should not be called'); _()}
+		], function(err) { assert.equal(err.message, 'someError'); done(); })();
+	});
+	it('should handle exceptions thrown by functions by calling done with the exception', function(done) {
+		util.seq([
+			function(_) { throw new Error('someError');},
+			function(_) {assert(0, 'This should not be called'); _()}
+		], function(err) { assert.equal(err.message, 'someError'); done(); })();
+	});
+	it('should call done with no error if all is successful', function(done) {
+		util.seq([
+			function(_) {setTimeout(_, 10);},
+			function(_) {setTimeout(_, 10);},
+			function(_) {setTimeout(_, 10);}
+		], done)();
+	});
+});
+
+describe('timeUid()', function() {
+	it('should return a unique string', function() {
+		var vals = {};
+		for(var i = 0; i < 1000; i++) {
+			var tuid = util.timeUid();
+			assert.equal(typeof(tuid), 'string');
+			assert(!(tuid in vals), 'Value not unique');
+			vals[tuid] = 1;
+		}
+	});
+	it('should return a larger value when called over one millisecond later', function(done) {
+		var a, b;
+		util.seq([
+			function(_) { a = util.timeUid(); setTimeout(_, 2); },
+			function(_) { b = util.timeUid(); setTimeout(_, 2); },
+			function(_) { assert(b > a, 'Later value is not larger than earlier'); _();},
+		], done)();
+	});
+});
+
--- util.js
+++ util.js
@@ -4,8 +4,7 @@ exports.protect = function(done, func) {
 		try {
 			return func.apply(this, arguments);
 		} catch(e) {
-			done(e);
-			throw e;
+			return done(e);
 		}
 	}
 }
@@ -22,3 +21,27 @@ exports.shouldFail = function(done, desc, func) {
 		}
 	}
 }
+
+function funcCompose(f, g) {
+	return function() {
+		f(g);
+	}
+}
+
+exports.seq = function(funcs, done) {
+	var f = done;
+	for(var i = funcs.length - 1; i >= 0; i--) {
+		f = exports.protect(done, funcCompose(funcs[i], f));
+	}
+	return f;
+}
+
+var MAX_UID = 0x1000000;
+
+exports.timeUid = function() {
+	var time = (new Date()).getTime().toString(16);
+	var uid = Math.floor((1 + Math.random()) * MAX_UID).toString(16);
+	uid = uid.substr(1); // The first character is always '1'
+	return time + uid; // string concatenation
+}
+
