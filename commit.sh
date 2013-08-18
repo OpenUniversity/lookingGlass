1ff56cf0252e2dfaba701e9c226571cba96a4f0b
Boaz Rosenan <brosenan@gmail.com>
2013-08-18 13:27:12 +0300

    28c28 <   25 passing (254 ms) --- >   25 passing (324 ms)

--- commit.sh
+++ commit.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+mocha -R list | sed 's/:[^:]*$//' > .new.tr
+comment=`diff .old.tr .new.tr`
 mocha -R markdown > README.md
+mv .new.tr .old.tr
 git add .
-git commit -a
+git commit -a -m "$comment"
 git push origin master
 
